c1dd4cc02fb3860e158ab95b0d3ecb11d8f12035
kontorol <60671553+kontorol@users.noreply.github.com>
2022-12-23 20:49:09 +0000

    Pending changes exported from your codespace

--- build.sh
+++ build.sh
@@ -213,6 +213,8 @@ sudo iptables -t nat -A PREROUTING -i ${IFACE} -p udp --dport 53 -j REDIRECT --t
   # && sudo iptables -t nat -A PREROUTING -i ${IFACE} -p tcp --dport 443 -j REDIRECT --to-port 8080\
   # && sudo iptables -t nat -A PREROUTING -i ${IFACE} -p tcp --dport 80 -j REDIRECT --to-port 8080\
 log_action_end_msg $?
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8080\
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080\
 
 log_action_begin_msg "adding IPv6 iptables rules"
 sudo ip6tables -t nat -A PREROUTING -i ${IFACE} -p udp --dport 53 -j REDIRECT --to-port 5353\
